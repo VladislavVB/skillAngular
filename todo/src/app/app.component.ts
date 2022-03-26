15bf590e61e3c5255f7a51d1f1f0cc54ad2db5c1
Vlad Vlad <burdukovskij.vlad@mail.ru>
2022-03-27 00:16:11 +0300

    eeeee

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo';
+  todos: Todo[] = [];
+  newTodo: string;
+  saveTodo() {
+    if (this.newTodo) {
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isComplited = true;
+      this.todos.push(todo);
+      this.newTodo = '';
+    } else {
+      alert('Текст введи сучара');
+    }
+  }
+  done(id: number) {
+    this.todos[id].isComplited = !this.todos[id].isComplited;
+  }
+  remove(id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
 }
