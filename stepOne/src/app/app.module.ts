1ac2dc676015745efced74a0098262088ae69c55
Vlad Vlad <burdukovskij.vlad@mail.ru>
2022-03-26 22:51:35 +0300

    ssasd

--- stepOne/src/app/app.module.ts
+++ stepOne/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonModule } from './button/button.module';
-import { CardProductModule } from './card-product/card-product.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TooltipBoxModule } from './tooltip-box/tooltip-box.module';
 
 
 @NgModule({
   declarations: [AppComponent ],
-  imports: [BrowserModule, ButtonModule, CardProductModule, BrowserAnimationsModule, TooltipBoxModule],
+  imports: [BrowserModule, ButtonModule, BrowserAnimationsModule, TooltipBoxModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- stepOne/src/app/tooltip/tooltip.component.ts
+++ stepOne/src/app/tooltip/tooltip.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tooltip',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tooltip.component.scss']
 })
 export class TooltipComponent implements OnInit {
-
+  @Input() text = "Text tooltip"
   constructor() { }
 
   ngOnInit(): void {
